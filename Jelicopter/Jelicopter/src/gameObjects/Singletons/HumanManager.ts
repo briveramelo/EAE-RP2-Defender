a69d3334437a6735a666316c0e02e48ff1dc10c6
u1089885 <b.riveramelo@utah.edu>
2016-09-20 22:10:09 -0600

    Collision and bugs all resolved! All is well with this world. Except the Game Over Screen

--- Jelicopter/Jelicopter/src/gameObjects/Singletons/HumanManager.ts
+++ Jelicopter/Jelicopter/src/gameObjects/Singletons/HumanManager.ts
@@ -8,6 +8,7 @@
 
         isSaving: boolean = false;
         savePersonIndex: number;
+        personBeingCarried;
 
         constructor(game: Phaser.Game, level: Level01, people: Phaser.Group) {
             super(game, 0, 0, 'EnemyBullet');
@@ -19,47 +20,84 @@
         }
 
         update() {
-            this.checkToCollectPeople();
+            if (this.level.playerShip.alive) {
+                if (!this.isSaving) {
+                    this.checkToCollectPeople();
+                }
+                else {
+                    if (this.personBeingCarried.alive) {
+                        this.carryPerson();
+                        if (this.isOverlapping(this.level.playerShip, this.level.hospital)) {
+                            this.dropPerson();
+                            this.getPointsForPerson();
+                        }
+                    }
+                    else {
+                        this.dropPerson();
+                    }
+                }
+            }
+
         }
 
         checkToCollectPeople() {
-            for (var i = 0, len = this.people.children.length; i < len; i++) {
-                if (this.isOverlapping(this.level.playerShip, this.people.children[i])) {
+            var i: number = 0;
+            this.people.forEach(function (person: Person) {
+                if (person.myCollider.isColliding(this.level.playerShip.myCollider)) {
+                    this.personBeingCarried = person;
                     this.savePersonIndex = i;
                     this.isSaving = true;
                 }
-            }
+                i++;
+            }, this);
+            
+            //if (this.isSaving) {
+            //    var i = 0;
+            //    this.people.forEach(function (person) {
+            //        if (i == this.savePersonIndex) {
+            //            if (this.level.playerShip.scale.x === 1) {
+            //                person.body.x = this.level.playerShip.body.x + 30;
+            //                person.body.y = this.level.playerShip.body.y + 80;
+            //            }
+            //            else {
+            //                person.body.x = this.level.playerShip.body.x - 105;
+            //                person.body.y = this.level.playerShip.body.y + 80;
+            //            }
+            //        }
+            //        i++;
+            //    }, this);
+            //}
 
-            if (this.isSaving) {
-                var i = 0;
-                this.people.forEach(function (person) {
-                    if (i == this.savePersonIndex) {
-                        if (this.level.playerShip.scale.x === 1) {
-                            person.body.x = this.level.playerShip.body.x + 30;
-                            person.body.y = this.level.playerShip.body.y + 80;
-                        }
-                        else {
-                            person.body.x = this.level.playerShip.body.x - 105;
-                            person.body.y = this.level.playerShip.body.y + 80;
-                        }
-                    }
-                    i++;
-                }, this);
+        }
+
+        carryPerson() {
+            if (this.level.playerShip.scale.x === 1) {
+                this.personBeingCarried.body.x = this.level.playerShip.body.x + 30;
+                this.personBeingCarried.body.y = this.level.playerShip.body.y + 80;
             }
-            if (this.isOverlapping(this.level.playerShip, this.level.hospital)) {
-                if (this.isSaving) {
-                    this.isSaving = false;
-                    this.level.scoreboard.updateScore(10);
-                    var i = 0;
-                    this.people.forEach(function (person) {
-                        if (i == this.savePersonIndex) {
-                            person.kill();
-                            person.destroy();
-                        }
-                        i++;
-                    }, this);
-                }
+            else {
+                this.personBeingCarried.body.x = this.level.playerShip.body.x - 105;
+                this.personBeingCarried.body.y = this.level.playerShip.body.y + 80;
             }
+        }
+
+        dropPerson() {
+            this.isSaving = false;
+        }
+
+        getPointsForPerson() {
+            this.level.scoreboard.updateScore(10);
+            //var i = 0;
+            //this.people.forEach(function (person) {
+            //    if (i == this.savePersonIndex) {
+            //        person.kill();
+            //        person.destroy();
+            //    }
+            //    i++;
+            //}, this);
+            this.personBeingCarried.kill();
+            //this.personBeingCarried.destroy();
+            //this.personBeingCarried = null;
         }
 
         isOverlapping(spriteA, spriteB) {
