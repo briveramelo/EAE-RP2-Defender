64ec251c56e553bafb0dcc4cf0c5809142773cae
u1089885 <b.riveramelo@utah.edu>
2016-09-22 22:35:26 -0600

    fixed a pauser bug

--- Jelicopter/Jelicopter/src/gameObjects/Singletons/pauser.ts
+++ Jelicopter/Jelicopter/src/gameObjects/Singletons/pauser.ts
@@ -8,18 +8,20 @@
             super(game, 0, 0, 'EnemyBullet');
             this.game = game;
             game.add.existing(this);
+            this.game.input.keyboard.addCallbacks(this, this.unpause);
         }
 
         update() {
             if (this.game.input.keyboard.isDown(Phaser.Keyboard.P)) {
-                this.game.input.keyboard.addCallbacks(this, this.unpause);
                 this.game.paused = true;
             }
         }
 
         unpause(event) {            
-            if (this.game.input.keyboard.isDown(Phaser.Keyboard.P)){
-                this.game.paused = false;
+            if (this.game) {
+                if (this.game.input.keyboard.isDown(Phaser.Keyboard.P)){
+                    this.game.paused = false;
+                }
             }
         }
     }
