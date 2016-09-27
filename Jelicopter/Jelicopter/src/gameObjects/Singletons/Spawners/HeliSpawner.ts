bcc5c5c179444dd7fd36c472a28cb25c4685e139
u1089885 <b.riveramelo@utah.edu>
2016-09-27 17:49:33 -0600

    fixed spawning helicopters

--- Jelicopter/Jelicopter/src/gameObjects/Singletons/Spawners/HeliSpawner.ts
+++ Jelicopter/Jelicopter/src/gameObjects/Singletons/Spawners/HeliSpawner.ts
@@ -14,7 +14,9 @@
             for (var i: number = 0; i < shipsToSpawn; i++) {
                 var heli = this.level.helis.getFirstDead(false);
                 var playerX: number = this.level.playerShip.position.x;
-                heli.reset(this.game.rnd.between(playerX - this.level.backgroundImageWidth / 2, playerX + this.level.backgroundImageWidth / 2), this.game.rnd.between(this.level.heightOffset, this.level.gameHeight/2));
+                var spawnX: number = this.game.rnd.between(playerX - this.level.backgroundImageWidth / 2, playerX + this.level.backgroundImageWidth / 2);
+                var spawnY: number = this.game.rnd.between(this.level.heightOffset, this.level.gameSize.y / 2);
+                heli.reset(spawnX, spawnY );
                 heli.comeAlive();
             }            
         }
