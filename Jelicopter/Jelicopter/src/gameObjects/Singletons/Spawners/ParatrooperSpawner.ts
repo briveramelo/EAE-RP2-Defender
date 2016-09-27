11f8b7e820bc12b0a0851eac456ef8c81753d6aa
AjayBS <bsajay.22@gmail.com>
2016-09-27 16:10:38 -0600

    Changed method name in paratrooper spawner class.

--- Jelicopter/Jelicopter/src/gameObjects/Singletons/Spawners/ParatrooperSpawner.ts
+++ Jelicopter/Jelicopter/src/gameObjects/Singletons/Spawners/ParatrooperSpawner.ts
@@ -10,21 +10,13 @@
             this.level = level;
         }
 
-        spawnShips(paraTroopersToSpawn: number) {
+        spawnTroopers(paraTroopersToSpawn: number) {
             for (var i: number = 0; i < paraTroopersToSpawn; i++) {
                 var trooper = this.level.paratroopers.getFirstDead(false);
                 var playerX: number = this.level.playerShip.position.x;
                 trooper.reset(this.game.rnd.between(playerX - this.level.backgroundImageWidth / 2, playerX + this.level.backgroundImageWidth / 2), 0);
                 trooper.comeAlive();
             }
-            //for (var i: number = 0; i < (paraTroopersToSpawn); i++) {
-            //    var randomNumber = this.game.rnd.integerInRange(0, 60);
-            //    var person: ParaTrooper = this.level.paratroopers.getRandom(randomNumber, randomNumber);
-            //    var playerX: number = this.level.playerShip.position.x;
-            //    var spawnX: number = this.game.rnd.between(playerX - this.level.backgroundImageWidth / 2, playerX + this.level.backgroundImageWidth / 2);
-            //    var spawnY: number = person.floorNumber;
-            //    person.spawn(new Phaser.Point(spawnX, spawnY));
-            //}
         }
     }
 }
