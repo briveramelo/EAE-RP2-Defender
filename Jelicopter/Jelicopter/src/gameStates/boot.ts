abc75691c6bd4c07cb24e88530e3de8661bd96ba
BRANDON RIVERA-MELO <u1089885@ad.utah.edu>
2016-10-04 20:22:17 -0600

    not sure what's new. But the font problem isn't solved yet

--- Jelicopter/Jelicopter/src/gameStates/boot.ts
+++ Jelicopter/Jelicopter/src/gameStates/boot.ts
@@ -4,6 +4,10 @@
         preload() {
             //You can preload an image here if you dont want to use text for the loading screen
             this.game.load.text('PixelFont', 'assets/fonts/Kemco_Pixel_Bold.woff', true);
+            this.game.load.text('PixelFont', 'assets/fonts/Kemco_Pixel_Bold.ttf', true);
+            this.game.load.text('PixelFont', 'assets/fonts/Kemco_Pixel_Bold.svg', true);
+            this.game.load.text('PixelFont', 'assets/fonts/Kemco_Pixel_Bold.otf', true);
+            this.game.load.text('PixelFont', 'assets/fonts/Kemco_Pixel_Bold.eot', true);
         }
 
         create() {
